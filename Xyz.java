03e0e5a0b4d61c9fa6d5c2897f1c562c4e0e7ea2
Pankaj Anadure <pankaj.anadure@cognizant.com>
2021-02-12 12:15:22 +0530

    Modified Xyz.java

--- Xyz.java
+++ Xyz.java
@@ -2,10 +2,14 @@ public class Xyz
 {
 	public void demo()
 	{
-		System.out.println("Just for Demo");
+		System.out.println("Some text I modified here");
 	}
 	public void testMethod()
 	{
 		System.out.println("For Testing purpose");
+	}
+	public int add(int a, int b)
+	{
+		return a+b;
 	}
 }
